f58ca40843d9ee6148230ce169e51e2fbb526de8
alorabi5 <reda9808@gmail.com>
2025-06-17 18:20:18 -0700

    last changed

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const knowMoreBtn = document.querySelector('.about-us-btn');
 const scrollUp = document.getElementById('scroll-up');
+const allOurClientsOptions = document.querySelectorAll('.our-clientele-section-logos');
 
+
+// document.querySelector('.india-clients').style.display='';
+
+const clientsRigon = (event) => {
+    const selectedClassName = event.target.className;
+    const selected = document.querySelector(`.our-clientele-section-logos ${selectedClassName}`);
+
+    console.log(selected);
+
+    allOurClientsOptions.forEach(selectedList => {
+            selectedList.classList.add('hide-clients-list');
+    });
+
+    // selected.classList.remove('hide-clients-list');
+
+
+};
 
 window.addEventListener("scroll", () => {
     if(window.pageYOffset > 100)
