d33ad1a308ab30139dc2f61fd6d156e71109b9b4
alorabi5 <reda9808@gmail.com>
2025-06-19 15:47:17 -0700

    modal for every links

--- main.js
+++ main.js
@@ -12,8 +12,18 @@ const mobileNav = document.getElementById("mobile-burger-nav");
 const burgerMenu = document.getElementById("burger-menu-icon");
 
 // Modal Variables
-const modal = document.getElementById("know-more");
-const btn = document.getElementById("about-btn");
+const modalAbout = document.getElementById("about-modal");
+const modalProductDreem = document.getElementById("product-dreem-modal");
+const productDreemBtn = document.getElementById("produce-dreem-btn");
+
+const modalEquibase = document.getElementById("product-equibase-modal");
+const equibaseBtn = document.getElementById("product-equibase-btn");
+
+const modalEquityMonitor = document.getElementById("prodcut-equity-monitor-modal");
+const equityMonitorBtn = document.getElementById("prodcut-equity-monitor-btn");
+
+const modal = document.querySelectorAll(".modal");
+const aboutBtn = document.getElementById("about-btn");
 const closeModal = document.querySelectorAll(".close");
 
 
@@ -44,27 +54,36 @@ window.addEventListener("scroll", () => {
 
 });
 
-// Mobile Functions
-burgerMenu.onclick = () => {
-    mobileNav.style.display = "block";
-}
 
-closeModal[0].onclick = () => {
-    mobileNav.style.display = "none";
-}
+// Show / Hide Element Fucntions
+const showHideElement = (btn, el, status="none") => btn.onclick = () => el.style.display = status;
 
-
-// Modal Functions
-btn.onclick = () => {
-    modal.style.display = "block";
-}
-
-closeModal[1].onclick = () => {
-    modal.style.display = "none";
-}
-
-window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
+const outClickHideModal = (modalEl) => window.onclick = (event) => {
+    if (event.target == modalEl) {
+        modalEl.style.display = "none";
     }
 }
+
+// Mobile Functions
+showHideElement(burgerMenu, mobileNav, "block");
+
+showHideElement(closeModal[0], mobileNav);
+
+// Modal About Functions
+showHideElement(aboutBtn, modalAbout, "block");
+
+showHideElement(closeModal[1], modal[0]);
+
+// Products Functions
+
+// Dreem Products
+showHideElement(productDreemBtn, modalProductDreem, "block");
+showHideElement(closeModal[2], modal[1]);
+
+// Equibase Products
+showHideElement(equibaseBtn, modalEquibase, "block");
+showHideElement(closeModal[3], modal[2]);
+
+// Equity Monitor
+showHideElement(equityMonitorBtn, modalEquityMonitor, "block");
+showHideElement(closeModal[4], modal[3]);
